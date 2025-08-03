1e9c37a8bde680fd4727a8387f54c252d8a89992
ElectonAmpere <82173777+ElectronAmpere@users.noreply.github.com>
2025-08-03 14:28:03 +0530

    Added testcases to tshirts

--- test/test_tshirts.c
+++ test/test_tshirts.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include <assert.h>
+#include <limits.h>
 #include "../src/tshirts.h"
+
+#define SHIRTS_RANGE_SWEEP(start, stop, checker) \
+    for (int sweep = start; sweep <= stop; sweep++) \
+    {\
+        assert(size(sweep) == checker);\
+    }
+
+void testTshirtSizeSmall() {
+    SHIRTS_RANGE_SWEEP(0, 38, 'S');
+}
+
+void testTshirtSizeMedium() {
+    SHIRTS_RANGE_SWEEP(39, 42, 'M');
+}
+
+void testTshirtSizeLarge() {
+    SHIRTS_RANGE_SWEEP(43, INT_MAX, 'L');
+}
 
 int testTshirtSize() {
     printf("\nTshirt size test\n");
-    assert(size(37) == 'S');
-    assert(size(40) == 'M');
-    assert(size(43) == 'L');
+    testTshirtSizeSmall();
+    testTshirtSizeMedium();
+    testTshirtSizeLarge();
     printf("All is well (maybe!)\n");
     return 0;
 }
+
